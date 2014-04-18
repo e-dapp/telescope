74f7d789db67d6c7d5c552a63da179cd32cecc6f
Eric Dapp <dapper.ic@gmail.com>
2014-04-17 22:16:08 -0400

    update to use curses

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
+#!/usr'bin/python
 import serial
 import sys
-import pygame
-from pygame.locals import *
-import inputbox
+import time
+import curses
+from curses.textpad import Textbox, rectangle
+
 
 port = ''
 dec = ''
 ra = ''
-clock = pygame.time.Clock()
-
+current_info = [';']
 
 def open_port():
     port_name = '/dev/ttyUSB0'
-    port_name = inputbox.ask(screen, 
-                             "Set port to open [leave blank for '/dev/ttyUSB0']")
+    port_name = get_param("Set port to open [leave blank for '/dev/ttyUSB0']")
     if port_name == '':
         ser = serial.Serial('/dev/ttyUSB0', 19200, timeout = 0.1)
     else:
@@ -64,120 +64,98 @@ def help_box():
                          h_box_place[1] + 5 + i*20))
 
 def current_info_box():
-    box_place = (10, 10, screen.get_width() - 270, 300)
-    
-    fontobject = pygame.font.SysFont("monospace", 14)
-    
-    pygame.draw.rect(screen, black,
-                         box_place,
-                         0)
-    pygame.draw.rect(screen, white,
-                             box_place,
-                             1)
-    new_c_i = []
-    
-    screen.blit(fontobject.render('Current Information, [press u to update]',
-                                  1,(255,255,255)),
-                (box_place[0] + 10,
-                 box_place[1] + 5))    
-    
-    for i in range(len(current_info)):
-        x = manage_string(current_info[i])
-        new_c_i.append(x)
-    
-    for i in range(len(current_info_titles)):
-        screen.blit(fontobject.render(current_info_titles[i], 1,(255,255,255)),
-                    (box_place[0] + 10,
-                     box_place[1] + 40 + i*20))
-    
-    for i in range(len(current_info)):
-        screen.blit(fontobject.render(new_c_i[i], 1, (255,255,255)), 
-                    (box_place[0] + 300,
-                     box_place[1] + 40 + i*20))
+	if current_info == [';']:
+		pass
+	else:
+		new_c_i = []
+		for i in range(len(current_info)):
+			x = manage_string(current_info[i])
+			new_c_i.append(x)
+		for i in range(len(current_info)):
+			screen.addstr(i + 15, 35, new_c_i[i])
 
 def get_status():
-    port.readline()
-    port.write('!AGas;')
-    a = port.readline()
-    port.write('!AGai;')
-    b = port.readline()
-    port.write('!CGra;')
-    c = port.readline()
-    port.write('!CGde;')
-    d = port.readline()
-    port.write('!CGtr;')
-    e = port.readline()
-    port.write('!CGtd;')
-    f = port.readline()
-    
-    #return str(port.readline())
-    return [a, b, c, d, e, f]
+	port.readline()
+	port.write('!AGas;')
+	a = port.readline()
+	port.write('!AGai;')
+	b = port.readline()
+	port.write('!CGra;')
+	c = port.readline()
+	port.write('!CGde;')
+	d = port.readline()
+	port.write('!CGtr;')
+	e = port.readline()
+	port.write('!CGtd;')
+	f = port.readline()
+	
+	#return str(port.readline())
+	return [a, b, c, d, e, f]
 
 def manage_string(string):
-    new_string = ''
-    for i in string:
-        if i != ';':
-            new_string += i
-        else:
-            break
-    
-    return new_string
+	new_string = ''
+	for i in string:
+		if i != ';':
+			new_string += i
+		else:
+			break
+	
+	return new_string
 
-
-pygame.init()
-size = (width, height) = (800,500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("testing")
-myfont = pygame.font.SysFont("monospace", 16)
-black = (0, 0, 0)
-white = (255, 255, 255)
-
-good = True
-status_delay = 1000
-time_tracker = 0
+def get_param(prompt):
+	win = curses.newwin(5, 60, 5, 5)
+	win.border(0)
+	win.addstr(1,2,prompt)
+	return win.getstr(3,2,55).decode(encoding="utf-8")
 
 help_list = ['o - Open Port', 'e - Set Alignment Side', 
              'r - Target Right Ascension', 'd - Target Declination', 
              's - Set Target From RA/DE', 'a - Align from Target', 
-             'g - GoTo Target', 'u - Update Current Info']
+             'g - GoTo Target', 'u - Update Current Info',
+	     '------------','q - Exit']
 
 current_info_titles = ['Alignment State:', 'Side of the Sky:',
                        'Current Right Ascension:', 'Current Declination:',
                        'Target Right Ascension:', 'Target Declination:']
 
-current_info = [';']
+screen = curses.initscr()
+screen.timeout(50) #stops getch() from blocking
+start_time = time.time()
 
+good = True
 while good: 
-    screen.fill(black)
-    
-    help_box()
-    current_info_box()
-    
-    
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key ==pygame.K_ESCAPE:
-                good = False
-            if event.key == pygame.K_o:
-                port = open_port()
-            if event.key == pygame.K_d:
-                dec = inputbox.ask(screen, 
-                                   "Set target Declination [signed 6 digit]")
-            if event.key == pygame.K_r:
-                ra = inputbox.ask(screen, 
-                                  "Set target Right Ascension [unsigned 6 digit]")
-            if event.key == pygame.K_s:
-                assign_target(dec, ra)
-            if event.key == pygame.K_e:
-                direction = inputbox.ask(screen, 
-                                         "Set alignment side")
-                alignment_side(direction)
-            if event.key == pygame.K_a:
-                align_from_target()
-            if event.key == pygame.K_g:
-                goto_target()
-            if event.key == pygame.K_u:
-                current_info = get_status()
-    pygame.display.flip()
+	screen.clear()
+	screen.border(0)
+	screen.addstr(2, 2, "UTSC Python Telescope control system")
+	for i in range(len(help_list)):
+		screen.addstr(i + 3, 4, help_list[i])
+	for i in range(len(current_info_titles)):
+		screen.addstr(i + 15, 4, current_info_titles[i])
+	current_info_box()
+	current_time = time.time()
+	if port != '':
+		if current_time - start_time > 2:
+			current_info = get_status()
+	screen.refresh()
+	key = screen.getch()
+        if key == 27 or key == ord('q'): #27=ESC
+            good = False
+        if key == ord('o'):
+            port = open_port()
+        if key == ord('d'):
+            dec = get_param("Set target Declination [signed 6 digit]")
+        if key == ord('r'):
+            ra = get_param("Set target Right Ascension [unsigned 6 digit]")
+        if key == ord('s'):
+            assign_target(dec, ra)
+        if key == ord('e'):
+            direction = get_param("Set alignment side")
+            alignment_side(direction)
+        if key == ord('a'):
+            align_from_target()
+        if key == ord('g'):
+            goto_target()
+        if key == ord('u'):
+            current_info = get_status()
 
-pygame.quit()
+curses.endwin()
